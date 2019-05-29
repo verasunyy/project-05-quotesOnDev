cc629640ed6c7b2a5ba5adbd5f41e030635b0de2
verasunyy <verasun@live.com>
2019-05-29 11:43:03 -0700

    add some function for setting random post and in content.php get metadata for the quote

--- functions.php
+++ functions.php
@@ -57,7 +57,9 @@ add_filter( 'stylesheet_uri', 'qod_minified_css', 10, 2 );
  */
 function qod_scripts() {
 	wp_enqueue_style( 'qod-style', get_stylesheet_uri() );
-
+	//TODO add Font Awesome wp_enqueue_style, this is used for quotes
+	//try adding the quotes with css :: after and ::before psuedo element
+	//TODO add script.js file wp_enqueque_script
 	wp_enqueue_script( 'qod-starter-navigation', get_template_directory_uri() . '/build/js/navigation.min.js', array(), '20151215', true );
 	wp_enqueue_script( 'qod-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20151215', true );
 }
--- template-parts/content.php
+++ template-parts/content.php
@@ -5,14 +5,35 @@
  * @package QOD_Starter_Theme
  */
 
+ $source = get_post_meta(get_the_ID(), '_qod_quote_source', true);
+ $source_url = get_post_meta(get_the_ID(), '_qod_quote_source_url', true);
+
+// var_dump($source);
+// var_dump($source_url);
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</header><!-- .entry-header -->
-
 	<div class="entry-content">
-		<?php the_excerpt(); ?>
+		<?php the_content(); ?>
 	</div><!-- .entry-content -->
+
+	<div class="entry-meta">
+		<?php the_title('<span><h2 class="entry-title"> &mdash;', '</h2></span>');?>
+		<?php if($source && $source_url): ?>
+			<span class = "source">,<a href="<?php echo $source_url;?>">
+				<?php echo $source; ?>
+			</a></span>
+
+		<?php elseif($source):?>
+			<span class = "source">,
+				<?php echo $source; ?>
+			</span>
+		<?php else: ?>
+			<span class="source"></span>
+		<?php endif; ?>
+	</div>
 </article><!-- #post-## -->
+
+<?php if( is_home() || is_single()):?>
+	<button type="button" id="new-quote-button">Show Me Another!</button>
+<?php endif;?>
